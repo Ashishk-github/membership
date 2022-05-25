69aaa9940bda5f10b66f1fb3b899b950280d2326
Ashish K <ashishskabade@gmail.com>
2022-05-25 22:27:36 +0530

    2ndd

---
+++ demo.js
@@ -0,0 +1,23 @@
+/*console.dir(document);
+console.log(document.domain);
+console.log(document.title);
+console.log(document.URL);
+//document.title='hi';
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+console.log(document.getElementById('head-title'));
+var header=document.getElementById('head-title');
+header.textContent='Nope';
+header.innerText='yeppp';
+header.style.borderBottom='solid 3px #000';*/
+var titl=document.getElementsByClassName('title');
+titl[0].style.fontWeight='bold';
+titl[0].style.color='green';
+titl.style.fontWeight='bold';
+
